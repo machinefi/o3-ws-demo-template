3e3f26cfed22856678b3feeee350b92e2f6e609a
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-08-16 19:49:10 -0300

    remove repetitive code

--- applet/assembly/utils/eth-to-wei.ts
+++ applet/assembly/utils/eth-to-wei.ts
@@ -1,9 +1,3 @@
-export function ethToWei(eth: string): string {
-  const floatValue = stringToFloat(eth);
-  const floatWei = toPow18(floatValue);
-  return floatWeiToString(floatWei);
-}
-
 export function stringToFloat(value: string): f64 {
   const valueFloat = parseFloat(value);
 
@@ -12,16 +6,4 @@ export function stringToFloat(value: string): f64 {
   }
 
   return valueFloat;
-}
-
-function toPow18(value: f64): f64 {
-  for (let i = 0; i < 18; i++) {
-    value *= 10;
-  }
-  return value;
-}
-
-function floatWeiToString(value: f64): string {
-  const stringified = value.toString();
-  return stringified.split(".")[0];
 }
